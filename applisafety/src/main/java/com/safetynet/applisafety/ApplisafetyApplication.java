f5a2e28e091ff762dace2baf4d11955ccb670f9a
Fannie <fannie.reyt@gmail.com>
2024-01-10 19:05:35 +0100

    premier commit

---
+++ applisafety/src/main/java/com/safetynet/applisafety/ApplisafetyApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.applisafety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplisafetyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplisafetyApplication.class, args);
+	}
+
+}
---
+++ applisafety/src/test/java/com/safetynet/applisafety/ApplisafetyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet.applisafety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplisafetyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
